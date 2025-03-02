c424ff12347256e39b22d94200b9d25d6e3ddd50
Victor Moraes <dt.victor@gmail.com>
2025-03-02 23:06:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var member = {
+    name: 'Member',
+    skills: [
+      {name: 'Basic', level: 1},
+      {name: 'Intermediate', level: 2},
+      {name: 'Advanced', level: 3}
+    ]
+  };
+  return member;
+}
